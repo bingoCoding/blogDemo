ff889f5a818e2da23e42df7a88eec254938115fd
liwenbin <liwenbin@raintai.com>
2017-10-19 17:48:17 +0800

    update

--- src/main/java/com/lp/domain/ReversePath.java
+++ src/main/java/com/lp/domain/ReversePath.java
@@ -11,7 +11,7 @@ import lombok.ToString;
 @Setter
 @ToString
 public class ReversePath {
-    private String picFilePath;//图片文件所在基础路径,形如"/home/jcala/blog/static/img/"
+    private String picFilePath;//图片文件所在基础路径,形如"/home/bingo/blog/static/img/"
     private String picUrlPath;//图片的链接基础路径,形如"http://127.0.0.1:8090/static/img/"
     private String cssUrlPath;//css文件链接基础路径,形如"http://127.0.0.1:8090/static/css/"
     private String jsUrlPath;//js文件链接基础路径,形如"http://127.0.0.1:8090/static/js/"
