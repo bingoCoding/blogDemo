c26fa8a490866da53a5967ce79af7401b64e9c83
liwenbin <liwenbin@raintai.com>
2018-02-27 19:04:00 +0800

    test jenkins

---
+++ src/main/java/com/lp/config/SecrityConf.java
@@ -0,0 +1,10 @@
+package com.lp.config;
+
+
+import org.springframework.boot.autoconfigure.security.SpringBootWebSecurityConfiguration;
+import org.springframework.context.annotation.Configuration;
+
+//@Configuration
+//public class SecrityConf extends WebSecurityConfigurationAdaper{
+//
+//}
